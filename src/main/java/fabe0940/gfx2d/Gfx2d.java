0ec81c86f3bb77a2df6c28ea8649708140748050
Mason Fabel <fabe0940@vandals.uidaho.edu>
2016-02-11 22:48:57 -0800

    created main viewports

--- src/main/java/fabe0940/gfx2d/Gfx2d.java
+++ src/main/java/fabe0940/gfx2d/Gfx2d.java
@@ -14,9 +14,15 @@ public class Gfx2d {
 
 	public static Window initGraphics() {
 		Window w;
+		Viewport v;
 
 		w = new Window(new Point(100, 100), new Point(800, 600));
-		// w.addViewport(v)
+
+		v = new Viewport(new Point(0, 600), new Point(400, 600));
+		w.addViewport(v);
+
+		v = new Viewport(new Point(400, 600), new Point(400, 600));
+		w.addViewport(v);
 
 		return w;
 	}
