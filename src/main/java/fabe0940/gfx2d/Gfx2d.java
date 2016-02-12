0f129139b14e4799c42f9af4f2252461e19178b3
Mason Fabel <fabe0940@vandals.uidaho.edu>
2016-02-11 23:24:28 -0800

    sample drawing

--- src/main/java/fabe0940/gfx2d/Gfx2d.java
+++ src/main/java/fabe0940/gfx2d/Gfx2d.java
@@ -2,6 +2,7 @@ package fabe0940.gfx2d;
 
 import fabe0940.gfx2d.Window;
 import fabe0940.gfx2d.util.Point;
+import java.lang.Math.*;
 
 public class Gfx2d {
 	public static void main(String[] args) {
@@ -13,17 +14,41 @@ public class Gfx2d {
 	}
 
 	public static Window initGraphics() {
-		Window w;
+		final int STEPS = 1000;
+		int i;
+		double x;
+ 		double scaleX;
+		double scaleY;
+		double increment;
+		Point origin;
 		Viewport v;
+		Window w;
 
 		w = new Window(new Point(100, 100), new Point(800, 600));
 
-		v = new Viewport(new Point(0, 600), new Point(400, 600));
+		v = new Viewport(new Point(5, 595), new Point(390, 590));
+		v.moveTo2D(new Point(0, 0));
+		v.drawTo2D(v.getSize());
 		w.addViewport(v);
 
-		v = new Viewport(new Point(400, 600), new Point(400, 600));
+		v = new Viewport(new Point(405, 595), new Point(390, 590));
+		v.moveTo2D(new Point(0, 0));
+		v.drawTo2D(v.getSize());
+		w.addViewport(v);
+
+		v = new Viewport(new Point(100, 500), new Point(600, 400));
+		v.moveTo2D(new Point(0, 0));
+		v.drawTo2D(v.getSize());
 		w.addViewport(v);
 
 		return w;
+	}
+
+	public static double f(double x) {
+		return 3.0 * Math.exp(-0.33 * x) * Math.sin(Math.toRadians(3.0 * x));
+	}
+
+	public static double g(double x) {
+		return x;
 	}
 }
